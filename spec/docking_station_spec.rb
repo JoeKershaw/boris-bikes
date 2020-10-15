47693e2d25689aa85e0dff9d580846214243f5c1
olliekennedy <ollie.kennedy1@gmail.com>
2020-10-15 16:16:08 +0100

    challenge 20 done

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,6 +3,7 @@ require 'bike'
 
 
 RSpec.describe DockingStation do
+  let(:bike) { double :bike }
   describe '#initialize' do
     it 'takes a non-default capacity' do
       big_docking_station = DockingStation.new(30)
@@ -13,42 +14,39 @@ RSpec.describe DockingStation do
     end
   end
   describe '#release_bike' do
-    it 'docking station releases bike' do
-      subject.dock_bike(double(:bike)) #
-      expect(subject.release_bike).to be_instance_of Bike
+    it 'docking station releases bike if working' do
+      allow(bike).to receive(:working?).and_return true
+      subject.dock_bike(bike)
+      expect(subject.release_bike).to eq bike
     end
-    it 'releases a working bike' do
-      bike = double(:bike)
-      expect(bike.working?).to eq true
-    end
+#    it 'releases a working bike' do
+#      allow(bike).to receive(:working?).and_return true
+#      expect(bike.working?).to eq true
+#    end
     it 'raises an error if there are no bikes at the docking station' do
       expect { subject.release_bike }.to raise_error("No Bikes!")
     end
     it 'raises an error if the bike is broken' do
-      bike = double(:bike)
-      bike.report
+      allow(bike).to receive(:working?).and_return false
       subject.dock_bike(bike)
       expect { subject.release_bike }.to raise_error("This bike is broken!")
     end
   end
   describe '#dock_bike' do
     it 'raises an error if the docking station is full' do
-      bike = double(:bike)
       DockingStation::DEFAULT_CAPACITY.times {subject.dock_bike(bike)}
       expect { subject.dock_bike(bike)}.to raise_error("Docking Station is full!")
     end
     it 'docks bike at station' do
-      expect(subject.dock_bike(double(:bike))).to eq bike
+      expect(subject.dock_bike(bike)).to eq bike
     end
     it 'accepts any bike, broken or not' do
-      bike = double(:bike)
-      bike.report
+      allow(bike).to receive(:working?).and_return false
       expect(subject.dock_bike(bike)).to eq bike
     end
   end
 
   it '.bike: checks if the bike is at the dockingstation' do
-    bike = double(:bike)
     subject.dock_bike(bike)
     expect(subject.bikes[-1]).to eq bike
   end
